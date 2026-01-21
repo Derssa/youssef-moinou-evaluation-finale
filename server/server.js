b11da438f1b23566437d6acc73d3b06db4b55308
Derssa <youssefmoinou@gmail.com>
2026-01-21 15:02:42 -0500

    adding env config

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import tasksRoutes from "./routes/tasks.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(cors());
